15c64bdf1f5291428461577234f07002fb65f439
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-07-11 11:51:46 +0200

    run_cmp_gpu: module load CUDA

--- run_cmp_gpu.bash
+++ run_cmp_gpu.bash
@@ -6,6 +6,7 @@ export CUDA_VISIBLE_DEVICES=$GPU_ID
 module load MATLAB/9.7
 module load SuiteSparse/5.1.2-foss-2018b-METIS-5.1.0
 module load LLVM/6.0.0-GCCcore-7.3.0 # avoid pyrender/OSMesa crash
+module load CUDA/9.0.176-GCC-6.4.0-2.28
 export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:/datagrid/personal/lucivpav/gflags-2.2.2/build/lib:/datagrid/personal/lucivpav/InLocCIIRC_demo/functions/vlfeat/toolbox/mex/mexa64
 export INLOC_HW="GPU"
 cat startup.m inloc_demo.m | matlab -nodesktop -singleCompThread 2>&1 | tee InLocCIIRC_demo.log
