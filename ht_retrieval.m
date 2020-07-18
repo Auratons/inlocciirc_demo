3318f47966b4d9a84737f50b6e73bae6fb1ea266
Pavel Lučivňák <pavel.lucivnak@gmail.com>
2020-07-18 17:41:44 +0200

    support for reduced number of queries for quick experimentation

--- ht_retrieval.m
+++ ht_retrieval.m
@@ -15,11 +15,13 @@ if exist(top100_matname, 'file') ~= 2
     load(params.input.scores.path, 'score');
     
     %shortlist format
-    for ii = 1:1:size(score, 1)
-        ImgList(ii).queryname = query_imgnames_all{ii};
-        [~, score_idx] = sort(score(ii, :), 'descend');
-        ImgList(ii).topNname = cutout_imgnames_all(score_idx(1:shortlist_topN));
-        ImgList(ii).topNscore = score(ii, score_idx(1:shortlist_topN));
+    for i=1:size(query_imgnames_all,2)
+        queryName = query_imgnames_all{i};
+        ImgList(i).queryname = queryName;
+        ii = find(strcmp({score.queryname}, queryName));
+        [~, score_idx] = sort(score(ii).scores, 'descend');
+        ImgList(i).topNname = cutout_imgnames_all(score_idx(1:shortlist_topN));
+        ImgList(i).topNscore = score(ii).scores(score_idx(1:shortlist_topN));
     end
     
     if exist(params.output.dir, 'dir') ~= 7
