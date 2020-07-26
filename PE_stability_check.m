Auratons/inlocciirc_demo

commit 983d4c0b7945e9ca694c4075246b7f3d102d37ef

fix critical bug in parfor_densePE; evaluation performance now significantly improved

functions/wustl_function/parfor_densePE.m
@@ -57,17 +57,24 @@ if exist(this_densepe_matname, 'file') ~= 2
             transformation_txtname = fullfile(params.dataset.db.trans.dir, this_floorid, 'transformations', ...
                         sprintf('trans_%s.txt', info.scan_id));
             P = load_CIIRC_transformation(transformation_txtname);
+
             %Feature upsampling
             Idbsize = size(XYZcut);
             Iqsize = Idbsize; % we padded the queries to match cutout aspect ratio (and rescaled to cutout dimensions
-            % TODO: why are the next two lines necessary
             tent_xq2d = at_featureupsample(tent_xq2d,this_gvresults.cnnfeat1size,Iqsize);
+                % without this, the features in query image would not match the cutout aspect ratio
             tent_xdb2d = at_featureupsample(tent_xdb2d,this_gvresults.cnnfeat2size,Idbsize);
+                % this may not be necessary
             %query ray
+            
+            % convert xq2d to match original query image
+            queryWidth = params.camera.sensor.size(2);
+            queryHeight = params.camera.sensor.size(1);
+            cutoutWidth = Idbsize(2);
+            cutoutHeight = Idbsize(1);
+            tent_xq2d = adjust_inliers_to_match_original_query(tent_xq2d, queryWidth, queryHeight, cutoutWidth, cutoutHeight);
 
-            cutoutWidth = params.dataset.db.cutout.size(1);
-            cutoutHeight = params.dataset.db.cutout.size(2);
-            K = buildK(params.camera.fl, cutoutWidth, cutoutHeight);
+            K = params.camera.K;
 
             tent_ray2d = K^-1 * [tent_xq2d; ones(1, size(tent_xq2d, 2))];
             %DB 3d points

commit b7a50609f9ff736ee7cb6b0c6c198e45c837e4f1

fix bug for k>1 caused by 983d4c0b7945e9ca694c4075246b7f3d102d37ef

PE_stability_check.m
@@ -4,8 +4,8 @@
 
 %% script inputs - adjust accordingly
 queryMode = 'holoLens1';
-experimentName = 'HL1-v4-k3';
-segmentName = '200.jpg'; % the best choice from PV shortlist will be chosen
+experimentName = 'HL1-v4.2-k3';
+segmentName = '223.jpg'; % the best choice from PV shortlist will be chosen
 
 %% initialize
 setenv("INLOC_EXPERIMENT_NAME", experimentName);

functions/wustl_function/parfor_densePE.m
@@ -135,7 +135,6 @@ if exist(this_densepe_matname, 'file') ~= 2
             queryInd = [firstQueryId:lastQueryId]';
             cutoutWidth = params.dataset.db.cutout.size(1);
             cutoutHeight = params.dataset.db.cutout.size(2);
-            K = buildK(params.camera.fl, cutoutWidth, cutoutHeight);
             Ps = multiCameraPose(workingDir, queryInd, posesFromHoloLens, ...
                                                 allCorrespondences2D, allCorrespondences3D, ...
                                                 inlierThreshold, numLoSteps, ...